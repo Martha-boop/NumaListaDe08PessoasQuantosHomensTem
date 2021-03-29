c5af7c664e634a2ee1529a8b483ccdd4d6bfdf2d
Marta Neris <78042453+Martha-boop@users.noreply.github.com>
2021-03-29 20:18:46 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NumaListaDe08PessoasQuantosHomensTem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal quant = 0;
+            string sexo = "";
+            int i = 0;
+
+            Console.WriteLine("Verificar quantos sexo Masculino!");
+           
+
+            while(i < 8)
+            {
+                i = i + 1;
+                Console.WriteLine("Digite o sexo da pessoa!");
+                sexo = Console.ReadLine();
+                
+                if(sexo == "masculino")
+                {
+                    quant = quant + 1;
+                }
+            }
+            Console.WriteLine("Existem "    + quant+ " pessoas do sexo masculino nesta lista!");
+
+            Console.ReadLine();
+        }
+        
+    }
+}
